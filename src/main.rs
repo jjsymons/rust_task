32afa96bda1dfd3f802fc7706034d21cce492fff
Joe Symons <joe.j.symons@gmail.com>
2024-08-27 15:25:42 +0100

    Removed While loop to reduce amount of inputs. Added Exit. Worked on task delete and fixed get_input_int.. func

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,27 @@ fn main() {
 
     let mut task_vector: Vec<String> = vec![]; // Should probably change this to create a small csv
 
-    let mut continuation_bool = true;
-
-    while continuation_bool {
-        println!("Would you like to add a task (A), delete a task (B) or read your current tasks (C)?:\nPlease enter:\n'A'/'a', 'B'/'b' or 'C'/'c'");
+    loop {
+        println!("Would you like to add a task (A), delete a task (B), read your current tasks (C) or (E) to Exit?:\nPlease enter:\n'A', 'B', 'C' or 'E'");
         
         let mut answer = get_input_string();
         answer.make_ascii_uppercase();
 
         if answer == "A" {
             task_create(&mut task_vector);
-            continuation_bool = continuation_response_question();
             continue
         } if answer == "B" {
             task_delete(&mut task_vector);
-            continuation_bool = continuation_response_question();
             continue
         } if answer == "C" {
             // Display tasks
             task_read(&task_vector);
-            continuation_bool = continuation_response_question();
             continue
+        } if answer == "E" {
+            // Exits
+            break
         } else {
-            println!("A input of something other than A, B, or C given:\n'{answer}'");
+            println!("A input of something other than A, B, C or E given:\n'{answer}'");
             continue
         }
     }
@@ -46,8 +44,9 @@ fn task_create(task_vector: &mut Vec<String>) -> &Vec<String> {
 fn task_delete(task_list: &mut Vec<String>) {
     loop {
         println!("Please give the number of the task you wish to remove from the list");
+        println!("Please give an answer between 1 and {vector_size}.");
 
-        let mut answer = get_input_integer();
+        let answer = get_input_integer();
 
         let vector_size = task_list.len();
 
@@ -55,21 +54,22 @@ fn task_delete(task_list: &mut Vec<String>) {
             println!("Task list is empty, returning.");
             return 
         }
-
-        println!("Please give an answer between 1 and {vector_size}");
-        answer -= 1;
-
         if answer < 0 {
-            println!("Error your input was 0");
-        } else if answer > (vector_size -1) {
-            println!("Error you entered a value greater than {vector_size}");
-        } else if answer == (vector_size -1)  {
-            task_list.pop();
-        } else if answer > 0 {
-            task_list.remove(answer);
-        } else {
-            println!("ERROR: You entered a non-numberical value, please try again.");
+            println!("Value is less than 0.");
             continue
+        }
+        let mut answer: usize = answer.try_into().unwrap();
+
+
+        answer -= 1;
+        println!("{vector_size}");
+
+        if answer > (vector_size - 1) {
+            println!("Error you entered a value greater than {vector_size}.");
+        } else if answer == (vector_size - 1)  {
+            task_list.pop();
+        } else {
+            task_list.remove(answer);
         }
         return
     }
@@ -101,7 +101,7 @@ fn get_input_string() -> String {
     };
     input
 }
-fn get_input_integer() -> usize {
+fn get_input_integer() -> isize {
     // Function to get input from user when needed
     loop {
         let mut input = String::new();
@@ -109,28 +109,10 @@ fn get_input_integer() -> usize {
             .read_line(&mut input)
             .expect("Failed to get result");
 
-    let input: usize = match input.trim().parse() {
-        Ok(input) => return input,
-        Err(_) => {println!("ERROR, Please give a new input: "); continue},
-        };
+        let _input: isize = match input.trim().parse() {
+            Ok(input) => return input,
+            Err(_) => {println!("ERROR, Please give a new numberic input: "); continue},
+            };
     } 
-}
-
-fn continuation_response_question() -> bool {
-    // function to see if the program should exit, returning a bool to a while loop in the main
-    loop {
-        println!("Would you like to continue?:\n'Y'/'y' for yes or 'N'/'n' for no.");
-        let mut str = get_input_string();
-        str.make_ascii_uppercase();
-
-        if str == "N" {
-            return false
-        } else if str == "Y" {
-            return true
-        } else {
-            println!("An input other than 'Y'/'y' or 'N'/'n'\nInput was: {str}");
-            continue
-        }
-    }
 }
 
